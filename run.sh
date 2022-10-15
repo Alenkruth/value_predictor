3d3870211500a2d9fd9ca19bc5563854a213266c
alenkruth <alenkruth@gmail.com>
2022-10-14 21:29:38 -0400

    update script to parallelize run

--- run.sh
+++ run.sh
@@ -29,30 +29,25 @@ logdir="./logs/$timestamp"
 # echo $logdir
 
 # running the traces with the updated predictor
-echo "running compute int 18" 
-./cvp -v -t 1 ./traces/compute_int_18.gz &> $logdir/compute_int_18.txt
-echo "running compute int 25"
-./cvp -v -t 1 ./traces/compute_int_25.gz &> $logdir/compute_int_25.txt
-echo "running compute int 26"
-./cvp -v -t 1 ./traces/compute_int_26.gz &> $logdir/compute_int_26.txt
-echo "running compute int 27"
-./cvp -v -t 1 ./traces/compute_int_27.gz &> $logdir/compute_int_27.txt
-echo "running compute int 9"
-./cvp -v -t 1 ./traces/compute_int_9.gz &> $logdir/compute_int_9.txt
-echo "running srv 0"
-./cvp -v -t 1 ./traces/srv_0.gz &> $logdir/srv_0.txt
-echo "running srv 10"
-./cvp -v -t 1 ./traces/srv_10.gz &> $logdir/srv_10.txt
-echo "running srv 20"
-./cvp -v -t 1 ./traces/srv_20.gz &> $logdir/srv_20.txt
-echo "running srv 40"
-./cvp -v -t 1 ./traces/srv_30.gz &> $logdir/srv_40.txt
+echo "running compute int 18" && ./cvp -v -t 1 ./traces/compute_int_18.gz &> $logdir/compute_int_18.txt &
+echo "running compute int 25" && ./cvp -v -t 1 ./traces/compute_int_25.gz &> $logdir/compute_int_25.txt &
+echo "running compute int 26" && ./cvp -v -t 1 ./traces/compute_int_26.gz &> $logdir/compute_int_26.txt &
+echo "running compute int 27" && ./cvp -v -t 1 ./traces/compute_int_27.gz &> $logdir/compute_int_27.txt &
+echo "running compute int 9" && ./cvp -v -t 1 ./traces/compute_int_9.gz &> $logdir/compute_int_9.txt &
+echo "running srv 0" && ./cvp -v -t 1 ./traces/srv_0.gz &> $logdir/srv_0.txt &
+echo "running srv 10" && ./cvp -v -t 1 ./traces/srv_10.gz &> $logdir/srv_10.txt &
+echo "running srv 20" && ./cvp -v -t 1 ./traces/srv_20.gz &> $logdir/srv_20.txt &
+echo "running srv 40" && ./cvp -v -t 1 ./traces/srv_30.gz &> $logdir/srv_40.txt
+
+wait
 
 echo "finished running the traces with the updated value predictor"
 # grepping for the results
 
 echo "grepping the results"
-grep -nH -e "IPC" -r ./logs/$timestamp/*.txt
+grep -nH -e "IPC" -r ./logs/$timestamp/*.txt 2>&1 | tee $logdir/grepIPC.txt
+
+python3 getIPC.py
 
 echo "done"
 #for trace in ./traces/*; do
