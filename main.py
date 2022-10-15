3450bd98188f61a969c422859a85ee61d7331e01
alenkruth <alenkruth@gmail.com>
2022-10-15 16:53:33 -0400

    update files to calculate size and fix minor bugs

--- main.py
+++ main.py
@@ -15,15 +15,30 @@ import os
 import sys
 
 def averageIPC(dirname):
+    return_list = []
+    # calculating average IPC across workloads
     ipc_list = []
     curr_dir = os.path.abspath(os.getcwd())
-    grep_path = os.path.join(curr_dir, "logs/{0}/grepIPC.txt".format(dirname))
-    with open(grep_path) as grepfile:
+    grep_ipc_path = os.path.join(curr_dir, "logs/{0}/grepIPC.txt".format(dirname))
+    with open(grep_ipc_path) as grepfile:
         for line in grepfile:
             ipc_list.append(float(line.split("= ")[1]))
     average_IPC = sum(ipc_list)/len(ipc_list)
-    return average_IPC
+    # calculating size of the predictor in KB
+    grep_size_path = os.path.join(curr_dir, "logs/{0}/grepSize.txt".format(dirname))
+    with open(grep_size_path) as grepfile:
+        for line in grepfile:
+            predictor_size_string = (line.split(" ||| ")[1]).split(": ")[1]
+            predictor_size_bits = int(predictor_size_string.split(" bits")[0])
+    predictor_size_bytes = (predictor_size_bits/8)
+    predictor_size_kbytes = (predictor_size_bytes/1024)
+    
+    # return values
+    return_list.append(average_IPC)
+    return_list.append(predictor_size_kbytes)
+    return return_list
 
 run_dirname = sys.argv[1]
-mean_IPC = averageIPC(run_dirname)
-print(f"The average IPC is {mean_IPC}")
+returns = averageIPC(run_dirname)
+print(f"The average IPC is {returns[0]}")
+print(f"The predictor size is {returns[1]} kB")
